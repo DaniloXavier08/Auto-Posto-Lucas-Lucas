e117f5ce072bd54b1571c66533fe566499cbbd21
a162007X <a162007X@L2-13.hto.ifsp.edu.br>
2017-08-08 20:06:45 -0300

    Iniciei o código.

---
+++ AutoPosto/src/autoposto/AutoPosto.java
@@ -0,0 +1,22 @@
+/*
+ * Trabalho POO - IFSP
+ * Auto Posto Lucas&Lucas
+ * 
+ * Esse trabalho tem como objetivo criar
+ * um sistema de controle de um Auto Posto.
+ */
+
+package autoposto;
+
+/**
+ * Danilo  - 162007X
+ * William - 1620444
+ */
+public class AutoPosto {
+
+    
+    public static void main(String[] args) {
+        // Nothing yet.
+    }
+    
+}
